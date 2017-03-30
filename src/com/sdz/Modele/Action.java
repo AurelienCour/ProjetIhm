8aa86c4afc4c18350174924c317b3726369e487f
ARGONIS <ARGONIS@172.18.29.159>
2017-03-30 12:15:00 +0200

    Début de JavaDoc des classes Action et Evaluation

--- src/com/sdz/Modele/Action.java
+++ src/com/sdz/Modele/Action.java
@@ -3,9 +3,33 @@ package com.sdz.Modele;
 /**
  * Action effectuée par l’élève pour réaliser l’exercice
  * composée d’une tortue logo et de l’action effectuée sous forme textuelle
- * @author Groupe8
+ * @author Erwan Mazureau
  * @version 30/03/2017
  */
 public class Action {
-
+	
+	private TortueG tortue;
+	private String action;
+	
+	public Action(TortueG to, String ac){
+		setTortue(to);
+		setAction(ac);
+	}
+	
+	public TortueG getTortue(){
+		return tortue;
+	}
+	
+	public void setTortue(TortueG t){
+		this.tortue = t;
+	}
+	
+	public String getAction(){
+		return action;
+	}
+	
+	public void setAction(String s){
+		this.action = s;
+	}
+	
 }
--- src/com/sdz/Modele/Evaluation.java
+++ src/com/sdz/Modele/Evaluation.java
@@ -2,9 +2,33 @@ package com.sdz.Modele;
 
 /**
  * Représente une Evaluation saisis par un professeur
- * @author Groupe8
+ * @author Erwan Mazureau
  * @version 30/03/2017
  */
 public class Evaluation {
-
+	
+	private String note;
+	private String commentaire;
+	
+	public Evaluation(String no, String co){
+		setNote(no);
+		setCommentaire(co);
+	}
+	
+	public String getNote(){
+		return(n);
+	}
+	
+	public void setNote(String n){
+		this.note = n;
+	}
+	
+	public String getCommentaire(){
+		return(c);
+	}
+	
+	public void setCommentaire(String c){
+		this.commentaire = c;
+	}
+	
 }
