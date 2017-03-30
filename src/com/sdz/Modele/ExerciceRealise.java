1a8ced18ecb53effb8033037bd5f33da2308e9a7
Aurelien <Aurelien@172.17.74.37>
2017-03-30 12:03:24 +0200

    Javadoc
    ExerciceRealise, Professeur, Tentative et Utilisateur

--- src/com/sdz/Modele/ExerciceRealise.java
+++ src/com/sdz/Modele/ExerciceRealise.java
@@ -1,5 +1,7 @@
 package com.sdz.Modele;
 
+import java.util.ArrayList;
+
 /**
  * Cette classe correspond aux exercice complété par les Eleves.
  * Ces exercices pourront par la suite être évulaué par le professeur
@@ -7,5 +9,85 @@ package com.sdz.Modele;
  * @version 30/03/2017
  */
 public class ExerciceRealise {
+
+	private ArrayList<Tentative> listeTentatives;
+	private boolean correct;
+	private Evaluation resultat;
+	private Exercice exerciceFait;
+	
+	/**
+	 * Constructeur de la classe ExerciceRealise
+	 * @param exerciceFait L'exercice qui a été réalisé
+	 */
+	public ExerciceRealise(Exercice exerciceFait) {
+		this.exerciceFait = exerciceFait;
+	}
+
+	/**
+	 * Permet de récupérer la liste des tentatives
+	 * @return La liste des tentatives
+	 */
+	public ArrayList<Tentative> getListeTentatives() {
+		return listeTentatives;
+	}
+
+	/**
+	 * permet de savoir si l'exercice à été corrigé
+	 * @return true si l'exercice à été corrigé
+	 */
+	public boolean isCorrect() {
+		return correct;
+	}
+
+	/**
+	 * Permet de dire si l'exercice à été corrigé ou non
+	 * @param correct true si l'exercice à été corrigé
+	 */
+	public void setCorrect(boolean correct) {
+		this.correct = correct;
+	}
+
+	/**
+	 * Permet de récupérer l'evaluation si l'exercice est corrigé
+	 * retourne null si il n'est pas corriger
+	 * @return L'evaluation si l'exercice est corrigé, null sinon
+	 */
+	public Evaluation getResultat() {
+		return resultat;
+	}
+
+	/**
+	 * Permet de modifier l'évaluation d'un exercice
+	 * met le booleen correct a true si il ne l'est pas déja
+	 * @param resultat L'evaluation de l'exercice
+	 */
+	public void corriger(Evaluation resultat) {
+		this.resultat = resultat;
+	}
+
+	/**
+	 * permet de récupérer l'exercice qui a été effectué
+	 * @return L'exercice effectué
+	 */
+	public Exercice getExerciceFait() {
+		return exerciceFait;
+	}
+
+	/**
+	 * Permet de rajouter un tentative a la liste si cette derniere n'est pas présente
+	 * @param e la tentative à ajouter
+	 * @return true si la tentative a été ajouté
+	 */
+	public boolean addTentative(Tentative e) {
+		return listeTentatives.add(e);
+	}
+
+	/**
+	 * Permet de savoir si la liste des tentatives est vide ou non
+	 * @return true si la liste est vide
+	 */
+	public boolean isEmpty() {
+		return listeTentatives.isEmpty();
+	}
 
 }
--- src/com/sdz/Modele/Utilisateur.java
+++ src/com/sdz/Modele/Utilisateur.java
@@ -5,6 +5,68 @@ package com.sdz.Modele;
  * @author Groupe8
  * @version 30/03/2017
  */
+/**
+ * @author Aurelien
+ *
+ */
 public abstract class Utilisateur {
+	
+	private String identifiant;
+	private String motDePasse;
+	private String nom;
+	private String prenom;
+	
+	/**
+	 * Constructeur de la classe utilisateur
+	 * @param identifiant	L'identifiant de l'utilisateur
+	 * @param motDePasse	Le mot de passe de l'utilisateur
+	 * @param nom			Le nom de l'utilisateur
+	 * @param prenom		Le prenom de l'utilisateur
+	 */
+	public Utilisateur (String identifiant, String motDePasse, String nom, String prenom){
+		
+	}
+	
+	/**
+	 * permet de récupérer l'identifiant de l'utilisateur
+	 * @return L'identifiant de l'utilisateur
+	 */
+	public String getIdentifiant(){
+		return identifiant;
+	}
+	
+	/**
+	 * permet de récupérer le mot de passe de l'utilisateur
+	 * @return	Le mot de passe de l'utilisateur
+	 */
+	public String getMotDePasse(){
+		return motDePasse;
+	}
+	
+	/**
+	 * permet de récupérer le nom de l'utilisateur
+	 * @return Le nom de l'utilisateur
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * permet de récupérer le prenom de l'utilisateur
+	 * @return Le prenom de l'utilisateur
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * Permet a l'utilisateur de modifier son mot de passe
+	 * @param motDePasse	Le nouveau mot de passe
+	 */
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+	
+	
 
 }
