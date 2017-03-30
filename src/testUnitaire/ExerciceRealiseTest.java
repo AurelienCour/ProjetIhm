7d1c6c115d33a5aa8fa8b65ffe3cd6072afea61e
Aurelien <Aurelien@172.17.74.37>
2017-03-30 12:24:51 +0200

    Création des premiers tests unitaires

---
+++ src/testUnitaire/ExerciceRealiseTest.java
@@ -0,0 +1,14 @@
+package testUnitaire;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExerciceRealiseTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
