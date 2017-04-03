5969dfc1104e5c71af7d198972b06091a28a51f7
ARGONIS <ARGONIS@172.18.29.159>
2017-04-03 08:49:10 +0200

    Specification plus approfondie de la classe Action

--- src/edu/ihm/noyau_fonctionnel/Action.java
+++ src/edu/ihm/noyau_fonctionnel/Action.java
@@ -4,7 +4,7 @@ package edu.ihm.noyau_fonctionnel;
  * Action effectuée par l’élève pour réaliser l’exercice
  * composée d’une tortue logo et de l’action effectuée sous forme textuelle
  * @author Erwan Mazureau
- * @version 30/03/2017
+ * @version 03/04/2017
  */
 public class Action {
 	
@@ -12,11 +12,13 @@ public class Action {
 	private String action; //dernière action effectuée par la tortue sous forme textuelle 
 	
 	/**
-	 * Constructeur de la classe Action
-	 * prend en paramètres un attribut de type TortueG 
-	 * et un attribut de type String
-	 * @param tortue La tortue sur laquel va etre effectué l'action
-	 * @param action	L'action qui a été effectué
+	 * Constructeur de la classe Action,
+	 * prend en paramètres un attribut de type TortueG,
+	 * si null alors on initialise avec une tortueG,
+	 * et un attribut de type String,
+	 * si null alors on initialise avec la valeur "avancer"
+	 * @param tortue : la tortue sur laquelle va être effectué l'action
+	 * @param action : l'action qui a été effectuée
 	 */
 	public Action(TortueG tortue, String action){
 
