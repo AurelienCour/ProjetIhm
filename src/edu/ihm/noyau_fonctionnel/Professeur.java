96dc54b2b7a0008d174c7248a3340cf9678efe55
ClementSamnael <salingak17@hotmail.fr>
2017-04-03 09:47:27 +0200

    Specification de la classe Professeur

--- src/edu/ihm/noyau_fonctionnel/Professeur.java
+++ src/edu/ihm/noyau_fonctionnel/Professeur.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 /**
  * Classe heritant de la classe Utilisateur. Cette classe represente un enseignant, qui gere des classes
- * @author AurelienCourtillat
+ * @author AurelienCourtillat 
  * @version 30/03/2017
  */
 public class Professeur extends Utilisateur{
