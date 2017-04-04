597b8155f9257ba63420dd0213769a10475b78de
ARGONIS <ARGONIS@172.18.29.159>
2017-04-04 16:08:00 +0200

    EvaluationTest Fait

--- src/testUnitaire/EvaluationTest.java
+++ src/testUnitaire/EvaluationTest.java
@@ -2,13 +2,36 @@ package testUnitaire;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import edu.ihm.noyau_fonctionnel.Evaluation;
+import edu.ihm.noyau_fonctionnel.Exercice;
 
 public class EvaluationTest {
 
+	private Evaluation Eval;
+	
+	@Before
+    public void setUp(){
+    	Eval = new Evaluation("laNote", "leCommentaire");
+    }
+	
+	@After
+    public void tearDown(){
+    }
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSetNote() {
+		Eval.setNote("newNote");
+		assertEquals("newNote", Eval.getNote());
+	}
+	
+	@Test
+	public void testSetCommentaire() {
+		Eval.setCommentaire("newCommentaire");
+		assertEquals("newCommentaire", Eval.getCommentaire());
 	}
 
 }
