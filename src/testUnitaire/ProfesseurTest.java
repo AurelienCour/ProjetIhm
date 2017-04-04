15f70437eb8c39bcbd89602a184e3531df9cf975
ARGONIS <ARGONIS@172.18.29.159>
2017-04-04 15:46:42 +0200

    ProfesseurTest fait

--- src/testUnitaire/ProfesseurTest.java
+++ src/testUnitaire/ProfesseurTest.java
@@ -2,13 +2,45 @@ package testUnitaire;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import edu.ihm.noyau_fonctionnel.Classes;
+import edu.ihm.noyau_fonctionnel.Professeur;
 
 public class ProfesseurTest {
 
+	private Professeur Prof;
+	private Classes Classe;
+	
+	@Before
+    public void setUp(){
+    	Prof = new Professeur("leId", "leMotDePasse", "leNom", "lePrenom");
+    	Classe = new Classes("leNomClasse");
+    }
+	
+	@After
+    public void tearDown(){
+    }
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddClasse() {
+		Prof.addClasses(Classe);
+		assertEquals(Classe,Prof.getClasses().get(0));
 	}
+	
+	@Test
+	public void testIsEmpty() {
+		assertEquals(true, Prof.isEmpty());
+	}
+	
+	@Test
+	public void testRemoveClass() {
+		Prof.addClasses(Classe);
+		Prof.removeClasses(Classe);
+		assertEquals(true,Prof.isEmpty());
+	}
+	
 
 }
